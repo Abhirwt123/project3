663dcfe4eaa1bdfe1cffa3515c88e27999962acf
Abhirwt123 <pardeeprawat1617@gmail.com>
2023-09-04 16:12:32 +0530

    files

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider4').bxSlider({
+      slideWidth: 300,
+      minSlides: 5,
+      maxSlides: 3,
+      moveSlides: 1,
+      slideMargin: 10
+    });
+  });
